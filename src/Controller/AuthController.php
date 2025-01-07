fbbac82a7695554e1b0237f9b55ee1db1bac3606
katarinamarj <katarinamarjanovic02@gmail.com>
2025-01-07 01:28:50 +0100

    .

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -6,25 +6,42 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\Security;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Repository\UserRepository;
 
 
 class AuthController extends AbstractController
 {
     
-
     #[Route('/api/login', name: 'api_login', methods: ['POST'])]
-    public function login(Security $security): JsonResponse
+    public function login(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher,JWTTokenManagerInterface $jwtManager): JsonResponse
     {
-        $user = $security->getUser();
 
-        if (!$user) {
-            return new JsonResponse(['error' => 'Unauthorized'], 401);
+        $data = json_decode($request->getContent(), true);
+        $username = $data['username'] ?? '';
+        $password = $data['password'] ?? '';
+
+         /** @var User|null $user */
+         $user = $userRepository->findOneBy(['username' => $username]);
+         if (!$user) {
+            return new JsonResponse(['error' => 'Invalid credentials (user not found)'], 401);
+         }
+
+
+         if (!$passwordHasher->isPasswordValid($user, $password)) {
+            return new JsonResponse(['error' => 'Invalid credentials (bad password)'], 401);
         }
 
+        $token = $jwtManager->create($user);
+
         return new JsonResponse([
-            'username' => $user->getUserIdentifier(),
-            'roles' => $user->getRoles(),
+            'token' => $token,
         ]);
+        
     }
 
 }
